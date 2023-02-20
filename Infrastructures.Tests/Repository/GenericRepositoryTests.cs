ce50e3658c31138fa389bf11937a8783b681ef61
huetruong100802 <huetruong100802@gmail.com>
2023-02-20 08:21:11 +0000

    HueTQ-Fix-Test-And-Add-Migration

--- Infrastructures.Tests/Repository/GenericRepositoryTests.cs
+++ Infrastructures.Tests/Repository/GenericRepositoryTests.cs
@@ -53,7 +53,7 @@ namespace Infrastructures.Tests.Repository
         [Fact]
         public async Task GenericRepository_GetByIdAsync_ShouldReturnCorrectData()
         {
-            var mockData = _fixture.Build<User>().Without(u => u.Syllabuses).Without(u => u.Role).Create();
+            var mockData = _fixture.Build<User>().Without(u => u.Syllabuses).Without(u => u.Role).Without(u => u.DetailTrainingClassParticipate).Create();
             await _dbContext.Users.AddRangeAsync(mockData);
 
             await _dbContext.SaveChangesAsync();
@@ -76,7 +76,7 @@ namespace Infrastructures.Tests.Repository
         [Fact]
         public async Task GenericRepository_AddAsync_ShouldReturnCorrectData()
         {
-            var mockData = _fixture.Build<User>().Without(u=>u.Syllabuses).Without(u=>u.Role).Create();
+            var mockData = _fixture.Build<User>().Without(u=>u.Syllabuses).Without(u=>u.Role).Without(u=>u.DetailTrainingClassParticipate).Create();
 
 
             await _genericRepository.AddAsync(mockData);
@@ -88,7 +88,7 @@ namespace Infrastructures.Tests.Repository
         [Fact]
         public async Task GenericRepository_AddRangeAsync_ShouldReturnCorrectData()
         {
-            var mockData = _fixture.Build<User>().Without(u => u.Syllabuses).Without(u => u.Role).CreateMany(10).ToList();
+            var mockData = _fixture.Build<User>().Without(u => u.Syllabuses).Without(u => u.Role).Without(u => u.DetailTrainingClassParticipate).CreateMany(10).ToList();
 
 
             await _genericRepository.AddRangeAsync(mockData);
---
+++ Infrastructures/Migrations/20230220074037_Add_TraingingClass_Location_DetailTrainingClassLocation.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructures.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_TraingingClass_Location_DetailTrainingClassLocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    LocationName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getutcdate()"),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    ModificationDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModificationBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletionDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeleteBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TrainingClasses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LocationID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getutcdate()"),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    ModificationDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModificationBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletionDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeleteBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrainingClasses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TrainingClasses_Locations_LocationID",
+                        column: x => x.LocationID,
+                        principalTable: "Locations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DetailTrainingClassParticipates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TrainingClassID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getutcdate()"),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    ModificationDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModificationBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletionDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeleteBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetailTrainingClassParticipates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DetailTrainingClassParticipates_TrainingClasses_TrainingClassID",
+                        column: x => x.TrainingClassID,
+                        principalTable: "TrainingClasses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DetailTrainingClassParticipates_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetailTrainingClassParticipates_TrainingClassID",
+                table: "DetailTrainingClassParticipates",
+                column: "TrainingClassID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetailTrainingClassParticipates_UserId",
+                table: "DetailTrainingClassParticipates",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrainingClasses_LocationID",
+                table: "TrainingClasses",
+                column: "LocationID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DetailTrainingClassParticipates");
+
+            migrationBuilder.DropTable(
+                name: "TrainingClasses");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+        }
+    }
+}
